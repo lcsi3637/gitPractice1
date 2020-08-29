5827fa2174555123977fd5cc193e2c2f12414b1a
barisbelke <barisbelkebb@gmail.com>
2020-08-29 19:45:39 -0400

    saban inci

--- src/tests/itsanewPracticeClass.java
+++ src/tests/itsanewPracticeClass.java
@@ -2,7 +2,7 @@ package tests;
 
 public class itsanewPracticeClass {
     public static void main(String[] args) {
-
+        System.out.println("saban inci ");
         System.out.println("its a new practice Class");
     }
 }
