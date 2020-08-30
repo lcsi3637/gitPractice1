931b2880fd2e2691cb599d40ed9e057405faa451
saban inci <64227008+sabaninci@users.noreply.github.com>
2020-08-29 20:03:06 -0400

    All old comments are deleted and added new one.
    simdi barista sira

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -4,12 +4,5 @@ public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
-  
-  // how is going 
-
-    // Baris nasil gidiyor
-    //nasilsin abi
-
-    // walla ne olsun ... 
-
+ //Saban's Command 
 }
